c66da1e7d41197f598d26a747acee8c2bceee256
Gavin-McG <gavinmcg@umich.edu>
2024-04-21 19:51:07 -0400

    crc32 additional options for decoding

--- include/crc32.h
+++ include/crc32.h
@@ -7,13 +7,23 @@
 
 using namespace std;
 
+//polynomial used for CRC table hash
+const uint32_t CRC_POLY = 0xEDB88320;
+//default starting value when generating crc value
+const uint32_t CRC_INITIAL = 0xffffffff;
+
 //generate a lookup table for a crc32 a checksum
 void generateCRC(uint32_t* crcTable);
 
 //get the CRC32 checksum from a stringstream and a provided lookup-table
-uint32_t getCRC32(stringstream &ss, const uint32_t* crcTable);
+uint32_t getCRC32(stringstream &ss, const uint32_t* crcTable, bool flip = true, uint32_t initial = CRC_INITIAL);
 //get the CRC32 checksum from a string and a provided lookup-table
-uint32_t getCRC32(const string &s, const uint32_t* crcTable);
+uint32_t getCRC32(const string &s, const uint32_t* crcTable, bool flip = true, uint32_t initial = CRC_INITIAL);
+
+uint32_t getCRC32(const char* s, size_t length, const uint32_t* crcTable, bool flip = true, uint32_t initial = CRC_INITIAL);
+
+
+uint32_t iterateCRC(uint32_t crc, uint8_t v, const uint32_t* crcTable);
 
 
 #endif
--- src/crc32.cpp
+++ src/crc32.cpp
@@ -1,31 +1,41 @@
 #include "../include/crc32.h"
 
-//polynomial used for CRC table hash
-const uint32_t crcPoly = 0xEDB88320;
 
 void generateCRC(uint32_t* crcTable) {
     for (int i=0;i<=255;++i) {
         uint32_t n = i;
         for (int j=0; j<8; ++j) {
-            n = (n&1)==0 ? n>>1 : (n>>1)^crcPoly;
+            n = (n&1)==0 ? n>>1 : (n>>1)^CRC_POLY;
         }
         crcTable[i] = n;
     }
 }
 
-uint32_t getCRC32(stringstream &ss, const uint32_t* crcTable) {
+uint32_t getCRC32(stringstream &ss, const uint32_t* crcTable, bool flip, uint32_t initial) {
     string s = ss.str();
-    uint32_t crc = 0xFFFFFFFF;
+    uint32_t crc = initial;
     for (uint8_t c : s) {
         crc = (crc>>8) ^ crcTable[(crc^c) & 0xFF];
     }
-    return crc ^ 0xFFFFFFFF;
+    if (flip) crc ^= 0xFFFFFFFF;
+    return crc;
 }
 
-uint32_t getCRC32(const string &s, const uint32_t* crcTable) {
-    uint32_t crc = 0xFFFFFFFF;
+uint32_t getCRC32(const string &s, const uint32_t* crcTable, bool flip, uint32_t initial) {
+    uint32_t crc = initial;
     for (uint8_t c : s) {
         crc = (crc>>8) ^ crcTable[(crc^c) & 0xFF];
     }
-    return crc ^ 0xFFFFFFFF;
+    if (flip) crc ^= 0xFFFFFFFF;
+    return crc;
+}
+
+uint32_t getCRC32(const char* s, size_t length, const uint32_t* crcTable, bool flip, uint32_t initial) {
+    uint32_t crc = initial;
+    for (size_t i=0; i<length; ++i) {
+        uint8_t c = s[i];
+        crc = (crc>>8) ^ crcTable[(crc^c) & 0xFF];
+    }
+    if (flip) crc ^= 0xFFFFFFFF;
+    return crc;
 }
